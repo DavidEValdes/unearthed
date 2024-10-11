b0212ca380e0fcdfe33a97a4793034be3a480541
DavidEValdes <36570117+DavidEValdes@users.noreply.github.com>
2024-10-10 20:13:19 -0400

    commit

--- server/config/dotenv.js
+++ server/config/dotenv.js
@@ -1,2 +1,11 @@
-import dotenv from 'dotenv'
-dotenv.config({ path: '../.env' })
+// File: /server/config/dotenv.js
+import dotenv from 'dotenv';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname for ES modules
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Configure dotenv with the correct path to the .env file
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
